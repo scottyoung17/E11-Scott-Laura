26e586372fbef3daff828d21518ef0a6e7874a0e
lauricehawatmeh <124214217+lauricehawatmeh@users.noreply.github.com>
2023-02-15 14:59:09 -0800

    Create test.py

---
+++ test.py
@@ -0,0 +1,47 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "91ca6a08-1195-4ca4-ba28-5132ad656d0b",
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import random \n",
+    "import time\n",
+    "import \n",
+    "\n",
+    "start_time = time.time\n",
+    "run_time = 10\n",
+    "itime = start_time\n",
+    "\n",
+    "while itime < start_time + run_time\n",
+    "    itime = time.time()\n",
+    "    idata = random.random()\n",
+    "    print (itime, idata)\n",
+    "    time.sleep(1)"
+   ]
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3 (ipykernel)",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "codemirror_mode": {
+    "name": "ipython",
+    "version": 3
+   },
+   "file_extension": ".py",
+   "mimetype": "text/x-python",
+   "name": "python",
+   "nbconvert_exporter": "python",
+   "pygments_lexer": "ipython3",
+   "version": "3.9.12"
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
