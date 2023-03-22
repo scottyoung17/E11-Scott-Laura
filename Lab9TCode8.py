1dcfd7b5ce24eed09e5d4ae1d87623dfadc4c283
scottyoung17 <124214226+scottyoung17@users.noreply.github.com>
2023-03-22 16:24:09 -0700

    Create Lab9TCode8.py

---
+++ Lab9TCode8.py
@@ -0,0 +1,51 @@
+import RPi.GPIO as GPIO
+from datetime import datetime
+import random
+import time
+import argparse
+import sys
+
+
+def my_callback(channel):
+    if GPIO.input(channel) == GPIO.LOW:
+        global count
+        count += 1
+        print(time.time())
+
+
+
+
+filename = sys.argv[1]
+intTime = int(sys.argv[2])
+f = open(filename+".csv","w")
+metaData = ["Time","Count"]
+for entry in metaData:
+    f.write(entry+',')
+f.write('\n')
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(6, GPIO.IN)
+##GPIO.setup(6, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.add_event_detect(6, GPIO.FALLING, callback=my_callback)
+try: 
+    count = 0 
+    startTime = time.monotonic()
+    endTime = startTime + intTime
+    now = datetime.now()
+    currentTime = now.strftime("%H:%M:%S")
+    while time.monotonic() < endTime:
+        time.sleep(1)
+
+        if time.monotonic() - startTime >= 10:
+            print('Counts in last 10 seconds: ', count)
+        data = [currentTime,count]
+        for idata in data:
+            f.write(str(idata)+',')
+        f.write('\n')
+        count = 0
+        startTime = time.monotonic()
+except KeyboardInterrupt:
+    GPIO.cleanup()
+f.close()
+
+ 
